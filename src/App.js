e5d0bc56de25e225d6fbb9531951072029436818
kimmybrindle <kimmybrindle@gmail.com>
2026-05-09 12:29:14 -0700

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Kimmy Brindle and is{" "}
         <a
