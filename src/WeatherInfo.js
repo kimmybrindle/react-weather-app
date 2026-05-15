bd738667e3164278c70f4d7446d13f6484763295
kimmybrindle <kimmybrindle@gmail.com>
2026-05-14 22:53:56 -0700

    added weather forecast page and basic structure

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,10 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon
-                code={props.data.icon}
-                alt={props.data.description}
-              />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
