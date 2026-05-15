ca05328c73092f63c3099c9f086a4a9653823168
kimmybrindle <kimmybrindle@gmail.com>
2026-05-14 23:18:58 -0700

    added forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,17 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "5c04157d3b5bt2eeaa66db333fod0d64";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
